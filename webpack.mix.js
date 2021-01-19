c5c5ad2da31baa400f9d0223185595aaf2e3d632
HESLLER HULLER <hesllerh@gmail.com>
2021-01-19 14:27:24 -0300

    Commit before implement the auth system

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+    .copy('resources/app-assets', 'public/app-assets/')
+    .copy('resources/assets', 'public/assets/')
+;
